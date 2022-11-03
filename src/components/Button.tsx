bc3719799a3f74371331f54948d5fb50f33a28cc
magnokf <mkfelipe.rj@gmail.com>
2022-11-03 14:27:50 -0300

    feat: button pronto

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,25 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base'
 
 interface Props extends IButtonProps {
 	title: string
+	type?: 'primary' | 'secondary'
+	
 	
 }
 
-export function Button({ title, ...rest }: Props) {
+export function Button({ title, type, ...rest }: Props) {
 	return (
-		<ButtonNativeBase {...rest}>
-			<Text>
+		<ButtonNativeBase
+			w="full"
+			h={14}
+			rounded={'sm'}
+			textTransform={'uppercase'}
+			bg={type === 'secondary' ? 'red.500' : 'yellow.500'}
+			_pressed={{ bg: type === 'secondary' ? 'red.600' : 'yellow.600' }}
+			_loading={{ _spinner: { color: 'black' } }
+			}
+			{...rest}>
+			<Text fontSize={'sm'} color={type==='secondary'? 'white':'black'} fontFamily={'heading'}
+			>
 				{title}
 			</Text>
 		</ButtonNativeBase>
--- src/screens/SigIn.tsx
+++ src/screens/SigIn.tsx
@@ -10,6 +10,8 @@ export function SigIn() {
 			<Button
 				title="ENTRAR COM GOOGLE"
 				leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+				type="secondary"
+				
 			
 			/>
 		
