d9c5833d2406a9f3c2ac85f8f0add7e8b4cf02d4
magnokf <mkfelipe.rj@gmail.com>
2022-11-03 14:45:32 -0300

    feat: button pronto

--- src/screens/SigIn.tsx
+++ src/screens/SigIn.tsx
@@ -1,19 +1,24 @@
-import { Center, Icon } from 'native-base'
+import { Center, Icon, Text } from 'native-base'
 import Logo from '../assets/logo.svg'
 import { Button } from '../components/Button'
 import { Fontisto } from '@expo/vector-icons'
 
 export function SigIn() {
 	return (
-		<Center flex={1} bg="gray.900">
+		<Center flex={1} bg="gray.900" p={7}>
 			<Logo width={212} height={40} />
 			<Button
+				mt={8}
 				title="ENTRAR COM GOOGLE"
 				leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
 				type="secondary"
-				
+			
 			
 			/>
+			
+			<Text color={'white'} textAlign={'center'} mt={4}>
+				Não utilizamos nenhuma informação além{'\n'} do seu e-mail para criação de sua conta.
+			</Text>
 		
 		</Center>
 	
