a3369b0906441bb5ed8ad7f3ccbde7924159cbf6
magnokf <mkfelipe.rj@gmail.com>
2022-11-03 14:09:04 -0300

    feat: configurando button

--- src/screens/SigIn.tsx
+++ src/screens/SigIn.tsx
@@ -1,9 +1,18 @@
-import { Center, Text, VStack } from 'native-base'
+import { Center, Icon } from 'native-base'
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button'
+import { Fontisto } from '@expo/vector-icons'
 
 export function SigIn() {
 	return (
 		<Center flex={1} bg="gray.900">
-			<Text color="white" fontSize={24} fontFamily="heading">Sign In/Entrar</Text>
+			<Logo width={212} height={40} />
+			<Button
+				title="ENTRAR COM GOOGLE"
+				leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+			
+			/>
+		
 		</Center>
 	
 	)
