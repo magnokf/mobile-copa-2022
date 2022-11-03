c9963cea07f970a29fcebc445465180e94ebdc66
magnokf <mkfelipe.rj@gmail.com>
2022-11-02 22:44:20 -0300

    chore: Entrar

--- src/screens/SigIn.tsx
+++ src/screens/SigIn.tsx
@@ -3,7 +3,7 @@ import { Center, Text, VStack } from 'native-base'
 export function SigIn() {
 	return (
 		<Center flex={1} bg="gray.900">
-			<Text color="white" fontSize={24} fontFamily="heading">Sign In</Text>
+			<Text color="white" fontSize={24} fontFamily="heading">Sign In/Entrar</Text>
 		</Center>
 	
 	)
